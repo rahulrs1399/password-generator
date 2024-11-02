1fc61967d0fecea57f9ad07201657ef4fa0841f6
rahulsaroj632 <rahulsaroj632@gmail.com>
2024-11-02 18:24:22 +0000

    Passwordgenerator

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        {/* password text and copy button */}
+        <div className="header">
+          <div className='password__text'>&$3jduf^h</div>
+          <button className='password__button'>COPY</button>
+        </div>
+        
+        {/* Character length */}
+        
+
+        {/* checkboxs */}
+        {/* Strength Check */}
+        {/* Generate button */}
+      </div>
+        
+    </>
+  )
+}
+
+export default App
